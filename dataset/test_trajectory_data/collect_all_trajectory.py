eee45f0f50025ab426bdd2510c25b299efaad521
Zenobiaql <zhangqilin8879@mail.ustc.edu.com>
2025-01-16 17:13:43 +0800

    data processing

--- dataset/test_trajectory_data/collect_all_trajectory.py
+++ dataset/test_trajectory_data/collect_all_trajectory.py
@@ -1,1 +1,35 @@
 import numpy as np
+from dataclasses import dataclass
+import draccus
+from pathlib import Path
+
+@dataclass
+class PreprocessConfig:
+    root: str
+    
+@draccus.wrap()
+def data_accumulator(cfg: PreprocessConfig)->None:
+    p = Path(cfg.root)
+    data = []
+    for subdir in p.iterdir():
+        if subdir.is_dir():
+            frame_file = subdir / 'frames.npy'
+            action_file  = subdir / 'action.npy'
+            instruction_file = subdir / 'instruction.txt'
+            
+            frames = np.load(frame_file)
+            actions = np.load(action_file)
+            with open(instruction_file, 'r') as f:
+                instruction = f.read()
+                
+            for i in range(len(actions)):
+                data_pack = {}
+                data_pack["dataset_name"] = "PIZZADATASET"
+                data_pack['action'] = [actions[i]]
+                data_pack["observation"] = {}
+                data_pack["observation"]["image_primary"] = [frames[i]]
+                data_pack["task"] = {}
+                data_pack["task"]["language_instruction"] = instruction
+                data.append(data_pack)
+                
+            print(data[0],'\n',data[-1])
